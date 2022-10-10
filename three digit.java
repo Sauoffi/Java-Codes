48ddfe6d1a080231be69cca25240e5af63933dc9
Saurabh Sharma <sram75410@gmail.com>
2022-10-10 11:29:05 +0530

    comit

--- three digit.java
+++ three digit.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-public static void main(String args[]){
-    Scanner num = new Scanner(System.in);
-    System.out.println("Enter the three digit number :")
-    int length = String.valueOf(num).length();
-    if (length<3){
-        System.out.println("Its valid!!");
-    }else{
-        System.out.println("Its invalid entry!!"+num);
+class three{
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter the three digit number :");
+        int length = String.valueOf(num).length();
+        if (length<3){
+            System.out.println("Its valid!!");
+        }else{
+            System.out.println("Its invalid entry!!"+num);
+        }
     }
 }
